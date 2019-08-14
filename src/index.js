619a77341a16c8f750aa168e2027f6717ddb88d5
Arvin Agas <arvagas@gmail.com>
2019-08-14 09:51:11 -0400

    moved location of provider and store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import App from './App'
+import { reducer } from './reducer/reducer'
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import 'bulma/css/bulma.css'
+import './styles.scss'
+
+const store = createStore(reducer)
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+)
