6822e6a472e1772c8fd85854cefe973906adce63
Arvin Agas <arvagas@gmail.com>
2019-08-14 21:10:35 -0400

    working with  react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App'
 import { reducer } from './reducer'
@@ -15,7 +16,9 @@ const rootElement = document.getElementById('root')
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </Provider>,
     rootElement
 )
